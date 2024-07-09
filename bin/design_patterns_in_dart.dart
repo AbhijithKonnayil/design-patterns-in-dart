3647950365ffb4845192cc6d918f6ad416a9f32f
Abhijith K <iamabhijith.k@gmail.com>
2024-07-09 12:38:43 +0530

    init project

---
+++ bin/design_patterns_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:design_patterns_in_dart/design_patterns_in_dart.dart' as design_patterns_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${design_patterns_in_dart.calculate()}!');
+}
